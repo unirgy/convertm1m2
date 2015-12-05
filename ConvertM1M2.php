769b343388c44e0c303deb2a0b9daded0881074e
Alan Storm <astorm@alanstorm.com>
2015-12-04 23:05:11 -0800

    Validate Magento 1 and Magento 2 folders further.

--- ConvertM1M2.php
+++ ConvertM1M2.php
@@ -247,10 +247,10 @@ class ConvertM1M2
         if (!is_dir($this->_env['source_dir'])) {
             $this->log('[ERROR] Invalid modules code source directory: ' . $this->_env['source_dir']);
         }
-        if (!is_dir($this->_env['mage1_dir'])) {
+        if (!is_dir($this->_env['mage1_dir']) || !is_dir($this->_env['mage1_dir'] . '/app/code/core' )) {
             $this->log('[ERROR] Invalid Magento 1.x directory: ' . $this->_env['mage1_dir']);
         }
-        if (!is_dir($this->_env['mage2_dir'])) {
+        if (!is_dir($this->_env['mage2_dir']) || !file_exists($this->_env['mage2_dir'] . '/app/bootstrap.php')) {
             $this->log('[ERROR] Invalid Magento 2.x directory: ' . $this->_env['mage2_dir']);
         }
 
@@ -2232,6 +2232,7 @@ EOT;
                 try {
                     $refl = new ReflectionClass($parentClass);
                 } catch (Exception $e) {
+                    debug_print_backtrace();
                     $this->log("[WARN] Reflection Exception: " . $e->getMessage());
                     $refl = false;
                 }
