266b3576564e657f785660992e1bf5415e2334c8
Alan Storm <astorm@alanstorm.com>
2016-01-15 13:02:44 -0800

    Fixing conflicts

--- tests/ConvertM1M2TestCase.php
+++ tests/ConvertM1M2TestCase.php
@@ -4,7 +4,6 @@ class ConvertM1M2TestCase extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $argv = ['--testrun'];
         require_once __DIR__ . '/../ConvertM1M2.php';
     }
 }
