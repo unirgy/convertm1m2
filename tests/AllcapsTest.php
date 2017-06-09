fc415b5a98e4e54a0043cab01a5e9a8b87fda2a1
Alan Storm <astorm@alanstorm.com>
2017-06-09 15:49:33 -0700

    Test cases for https://github.com/unirgy/convertm1m2/issues/40

---
+++ tests/AllcapsTest.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'ConvertM1M2TestCase.php';
+
+class AllCaps extends ConvertM1M2TestCase
+{
+    public function testAllcaps()
+    {
+        $object = new TestableConvertM1M2();
+        $contents = $object->convertCodeContents('<' . '?' . 'php' . "\n" . 
+            'class FOO_Module_TestController{}');
+     
+        $this->assertContains('Module\\', $contents);   
+        $this->assertNotContains('FOO_\\', $contents);   
+    }
+    
+    public function testMethodParsingNoCaps()
+    {
+        $object = new TestableConvertM1M2();
+        $contents = $object->convertCodeContents('<' . '?' . 'php' . "\n" . 
+            'class Foo_Module_TestController{ public function indexAction(){}}');        
+        $contents = $object->convertCodeParseMethods($contents, 'php');
+        $currentFile = $object->getCurrentFile();     
+        $this->assertTrue(count($currentFile['methods']));
+    }
+
+    public function testMethodParsingAllCaps()
+    {
+        $object = new TestableConvertM1M2();
+        $contents = $object->convertCodeContents('<' . '?' . 'php' . "\n" . 
+            'class FOO_Module_TestController{ public function indexAction(){}}');        
+        $contents = $object->convertCodeParseMethods($contents, 'php');
+        $currentFile = $object->getCurrentFile();     
+        $this->assertTrue(count($currentFile['methods']));
+    }    
+}
--- tests/ConvertM1M2TestCase.php
+++ tests/ConvertM1M2TestCase.php
@@ -5,6 +5,12 @@ class ConvertM1M2TestCase extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         require_once __DIR__ . '/../ConvertM1M2.php';
+        require_once __DIR__ . '/TestableConvertM1M2.php';
+    }
+    
+    public function testAvoidNoTestsFound()
+    {
+        $this->assertTrue(true);
     }
 }
 
